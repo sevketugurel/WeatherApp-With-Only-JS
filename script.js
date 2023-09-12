67fb76f74299f9f0e97229cccab7c117f9ab8755
Sevket Ugurel <118289177+sevketugurel@users.noreply.github.com>
2023-09-12 23:36:27 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+const InputTextDOM = document.getElementById("InputText")
+const btnDOM = document.querySelector(".btn")
+
+btnDOM.addEventListener('click',() => {
+    getData(InputTextDOM.value)
+})
+
+function getData(name){
+    const API = "4379929ae507f845dc52a29c54ea469d";
+    const baseURL = `https://api.openweathermap.org/data/2.5/weather?q=${name}&appid=${API}&units=metric&lang=tr`
+    fetch(baseURL).then(res =>res.json())
+        .then(data => {
+            console.log(data)
+            const {name,sys:{country},main:{temp,feels_like},weather:[{description}]} = data;
+
+            const cityDOM = document.querySelector("#sehir")
+            const sicaklikDOM = document.querySelector("#sicaklik")
+            const havaDurumuDOM = document.querySelector("#havaDurumu")
+            const hissedilenDOM = document.querySelector("#hissedilen")
+            const weatherİconDOM = document.querySelector(".weather-icon")
+            cityDOM.textContent = `${name},${country}`.toUpperCase();
+            sicaklikDOM.textContent = `${temp}°C`;
+            havaDurumuDOM.textContent = `${description}`.toUpperCase();
+            hissedilenDOM.textContent = `${feels_like}°C`;
+
+        })
+        .catch(err => console.warn(err))
+}
